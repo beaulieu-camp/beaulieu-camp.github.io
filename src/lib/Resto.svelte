5d6936afa6ee219e0940daf667641b0f122ae545
aquabx <tom.chauvel@hotmail.com>
2023-07-17 03:17:42 +0200

    ajout resto u

---
+++ src/lib/Resto.svelte
@@ -0,0 +1,53 @@
+<script>
+    import Card from "./Card.svelte"
+    import { onMount } from "svelte";
+
+    let data = []
+
+    onMount(async()=>{
+        const response = await fetch('api/resto')
+        data = await response.json()
+        temperature = data.temperature
+        code = data.code
+    })
+
+</script>
+
+<Card title="Resto U Beaulieu">
+    <div>
+        {#each data as resto}
+                
+                <table>
+                    <tr><td colspan="7">{resto["nom"]}</td></tr>
+                    <tr>
+                        {#each resto["ouverture"] as ouv }
+                            <td>
+                                {#if ouv = "000"}
+                                    ❌
+                                {:else}
+                                    ✅
+                                {/if}
+                            </td>
+                        {/each}
+                    </tr>
+                </table>
+        {/each}
+    </div>
+
+</Card>
+
+
+
+<style>
+
+
+    div {
+        flex:1;
+        display: flex;
+        flex-direction: column;
+
+        text-align: left;
+    }
+
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,13 @@
     import Weather from "../lib/Weather.svelte";
     import Map from "../lib/Map.svelte";
   import Salles from "$lib/Salles.svelte";
+  import Resto from "$lib/Resto.svelte";
 </script>
 
 
     <Weather></Weather>
     <Salles></Salles>
+    <Resto></Resto>
     <Map></Map>
 
 
