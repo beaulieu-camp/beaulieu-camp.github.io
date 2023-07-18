b46cf9c77fcc6d41d8f32160ac0bec1cb2f11882
aquabx <tom.chauvel@hotmail.com>
2023-07-18 04:56:33 +0200

    nouvelle recherche

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -3,7 +3,6 @@
     import { onMount } from "svelte";
     import "leaflet/dist/leaflet.css"
     import "leaflet-fullscreen/dist/leaflet.fullscreen.css"
-    import "leaflet-search/dist/leaflet-search.min.css"
     
     function changelayer(event,map){
         let style
@@ -14,16 +13,17 @@
             style = "light_nolabels"
         }
 
-        let layer = `https://{s}.basemaps.cartocdn.com/rastertiles/${style}/{z}/{x}/{y}{r}.png`
-        L.tileLayer(layer,{}).addTo(map);
+        let layer = `https://{s}.basemaps.cartocdn.com/rastertiles/${style}/{z}/{x}/{y}.png`
+        L.tileLayer(layer,{maxZoom:20}).addTo(map);
         
     }
 
 
     onMount(async() => {
         const L = await import("leaflet")
+
         await import("leaflet-fullscreen")
-        await import("leaflet-search")
+        let search = await import("./search.ts")
 
         const geojson = await import('./geojson.json')
 
@@ -33,6 +33,7 @@
             fullscreenControl: true,
         });
 
+
         let searchLayer = L.geoJSON(geojson).setStyle({ 
             weight: 0,
             opacity: 1,
@@ -41,23 +42,13 @@
             fillOpacity: 1
         }).addTo(map);
 
-        L.control.search({
-            layer: searchLayer,
-            initial: false,
-            propertyName: 'name',
-            buildTip: function(text, val) {
-                return '<a href="#">'+text+'</a>';
-            }
-        }).addTo(map)
-
+        let searchbar = new search.default(searchLayer,"name")
+        searchbar.addTo(map)
 
         let query = window.matchMedia('(prefers-color-scheme: dark)')
 
         changelayer(query,map)
         query.addEventListener('change', (e) => changelayer(e,map));
-
-
-
 
         const resto = L.divIcon({
             html: `<svg viewBox="0 0 1000 1000" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="500" cy="500" r="500" fill="white"/><path d="M692 244C676 244 564 276 564 420V532C564 567.3 592.7 596 628 596H660V724C660 741.7 674.3 756 692 756C709.7 756 724 741.7 724 724V596V484V276C724 258.3 709.7 244 692 244ZM340 260C340 251.8 333.9 245 325.7 244.1C317.5 243.2 310.2 248.6 308.4 256.5L278.1 392.8C276.7 399.1 276 405.5 276 411.9C276 457.8 311.1 495.5 356 499.6V724C356 741.7 370.3 756 388 756C405.7 756 420 741.7 420 724V499.6C464.9 495.5 500 457.8 500 411.9C500 405.5 499.3 399.1 497.9 392.8L467.6 256.5C465.8 248.5 458.3 243.2 450.2 244.1C442.1 245 436 251.8 436 260V394.2C436 399.6 431.6 404 426.2 404C421.1 404 416.9 400.1 416.4 395L403.9 258.6C403.2 250.3 396.3 244 388 244C379.7 244 372.8 250.3 372.1 258.6L359.7 395C359.2 400.1 355 404 349.9 404C344.5 404 340.1 399.6 340.1 394.2V260H340ZM388.3 412H388H387.7L388 411.3L388.3 412Z" fill="black"/></svg>`,
@@ -93,24 +84,9 @@
         }).addTo(map);
 
         navigator.geolocation.watchPosition((e) => {
-            console.log(e.coords,e,circle)
             circle.setLatLng([e.coords.latitude,e.coords.longitude])
         })
 
-        	// control that shows state info on hover
-        // const info = L.control();
-
-        // info.onAdd = function (map) {
-        //     this._div = L.DomUtil.create('div', 'info');
-        //     this._div.innerHTML = `<input>`;
-        //     this.update();
-        //     return this._div;
-        // };
-
-        // info.update = function (props) {
-        // };
-
-        // info.addTo(map);
     })
 
 </script>
---
+++ src/lib/search.ts
@@ -0,0 +1,44 @@
+export default class {
+    geojson
+    tag
+    constructor(geojson,tag) {
+        this.geojson = geojson
+        this.tag = tag
+
+        const info = L.control();
+
+        let search_this = this
+
+        info.onAdd = function(map) {
+            this._div = L.DomUtil.create('div', 'search leaflet-control');
+            this._div.innerHTML = `<input><div></div>`;
+            let div = this._div.children[1]
+            this._div.children[0].addEventListener("input",(e) => search_this.search(e,search_this,div,map))
+            return this._div;
+        };
+
+        return info
+    }
+
+    search(e,ctx,div,map) {
+        div.innerHTML = ""
+        if (! e.target.value) return
+
+
+        let filtered = Object.values(ctx.geojson._layers).filter(
+            (layer) => layer.feature.properties["name"].toUpperCase().includes(e.target.value.toUpperCase()) 
+        )
+
+
+        for ( let layer of filtered.splice(0,10)){
+            let a = document.createElement("a")
+            a.style.display="block"
+
+            a.addEventListener("pointerdown",(e) => map.fitBounds(layer.getBounds())) ;
+            a.href="#"
+            a.innerText = layer.feature.properties["name"]
+            div.appendChild(a)
+        }
+
+    }
+}
