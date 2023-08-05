8b9d3dd133674b6b38280a71e6c3ed7d64fc8c5a
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2023-08-05 19:22:37 +0200

    footer mieux

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import Icons from "$lib/Icons.svelte"
+</script>
+
+<footer>
+	<a href="https://discord.gg/4T6CnF2qDF"><Icons name="discord"/></a>
+	<a href="https://github.com/beaulieu-camp"><Icons name="github"/></a>
+	<h4>Made with ❤️ by <a href="https://www.aquabx.ovh">AquaBx</a></h4>
+	<h4><a href="/infos">Mentions légales</a></h4>
+</footer>
+
+<style>
+	
+	footer {
+		display: flex;
+		width: 100%;
+		gap:2em;
+		justify-content: center;
+		align-items: center;
+		position: static;
+		bottom: 0;
+	}
+
+</style>
